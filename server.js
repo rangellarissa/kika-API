d9c97ed19a18a21c1e888fbd60d0c192c7cbcae1
larissa.rangel <larissa.rangel@MacBook-Air.local>
2023-04-27 16:58:09 -0300

    create service with allow cors

---
+++ server.js
@@ -0,0 +1,26 @@
+import { createServer } from 'http';
+import { readFile } from 'fs';
+
+const server = createServer((req, res) => {
+  // Set the content type to JSON
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', 'application/json');
+
+  // Read the contents of the JSON file
+  fs.readFile('example.json', 'utf8', (err, data) => {
+    if (err) {
+      // Handle any errors that occur while reading the file
+      res.statusCode = 500;
+      res.end(JSON.stringify({ error: 'Internal server error' }));
+      return;
+    }
+
+    // Return the contents of the JSON file as the response
+    res.end(data);
+  });
+});
+
+const port = process.env.PORT || 4000;
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
