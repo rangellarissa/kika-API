256cfc7812b66c1dab15b28c10b34b4cbea63563
Larissa Rangel <72013882+rangellarissa@users.noreply.github.com>
2026-03-01 15:25:17 -0300

    add new tables

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
-const Tables = ["admin", "artista", "exposicao", "imagem", "newsletter", "novidade", "obra", "usuario"];
+const Tables = ["admin", "artista", "exposicao", "imagem", "newsletter", "novidade", "obra", "usuario", "residencia", "research"];
 
 module.exports = {
   Tables
